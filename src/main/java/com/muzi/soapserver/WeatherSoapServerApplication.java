cab2ec503256ed78640e0b052e9ec0ad41246189
muzi-debugger <mmzolodev@gmail.com>
2025-04-22 18:55:37 +0200

    Initial commit

---
+++ src/main/java/com/muzi/soapserver/WeatherSoapServerApplication.java
@@ -0,0 +1,13 @@
+package com.muzi.soapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSoapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSoapServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muzi/soapserver/WeatherSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muzi.soapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSoapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
